c3756515398fc4e1f1b04b3854ef87aec5124bfe
AAMIR JAMAL <amirjamal496@gmail.com>
2024-08-19 11:08:00 +0500

    SQL Changes

--- Sql/dashboardregexbyte.sql
+++ Sql/dashboardregexbyte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2024 at 06:39 PM
+-- Generation Time: Aug 19, 2024 at 07:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,9 +39,33 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `status`, `description`) VALUES
-(8, 'Service 1', '2', 'Service Details Text Area'),
-(9, 'Service 2', '2', 'nsdkdkj'),
-(10, 'Service 3', '2', 'Service text area');
+(54, '4343', '1', '4343'),
+(55, 'knk', '1', 'gcfgh'),
+(56, 'fwew', '0', 'fweew'),
+(57, 'fwew', '0', 'fweew');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `profile` varchar(20) NOT NULL,
+  `id` int(10) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`profile`, `id`, `title`, `status`, `description`) VALUES
+('user.png', 1, 'Slider 1', '1', 'Slider One'),
+('edit1.png', 2, 'Slider 2', '1', 'Slider Second'),
+('logo.png', 5, 'Slider 3', '1', 'Slider Three');
 
 -- --------------------------------------------------------
 
@@ -54,7 +78,7 @@ CREATE TABLE `user-table` (
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `Password` varchar(200) NOT NULL,
+  `Password` varchar(100) NOT NULL,
   `phone` int(10) NOT NULL,
   `image` varchar(100) NOT NULL,
   `status` varchar(40) NOT NULL,
@@ -68,7 +92,7 @@ CREATE TABLE `user-table` (
 INSERT INTO `user-table` (`id`, `firstname`, `lastname`, `email`, `Password`, `phone`, `image`, `status`, `created-at`) VALUES
 (18, 'Majid', 'khan', 'm@gmail', '$2y$10$xbOcA2m/AgQU.bih8u0JUOV3WtYIExVXzCRLc.PLr7I3kLt0vItym', 232323, 'user.png', 'inactive', '0000-00-00 00:00:00'),
 (19, 'Majid', 'khan', 'm1@gmail', '$2y$10$BijaBi3bGEiue2drIc/htOdfv9yrtnnl0vnDD.fZf4j4JPfa8tpDu', 12121212, 'user.png', 'active', '2021-12-12 00:00:00'),
-(20, 'M', 'kqs`', 'nk@gmail', '', 121212, 'Back Arrow.png', 'Active', '2112-02-21 00:00:00');
+(27, ',mw', 'qlldmk', 'Q@gmail', '$2y$10$4al6ioGgjzAL/jS6sgNgeOeGtBP0guL03j.et9IYVthbIpDNJk1BO', 12121212, 'RBlogo.jfif', 'active', '1221-12-12 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -78,6 +102,12 @@ INSERT INTO `user-table` (`id`, `firstname`, `lastname`, `email`, `Password`, `p
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -95,13 +125,19 @@ ALTER TABLE `user-table`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user-table`
 --
 ALTER TABLE `user-table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
